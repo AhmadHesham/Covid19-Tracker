3bf989577f7c5be8d3739ebc7aad6a64bd7e6734
AhmadHesham <ahmedhsaid@gmail.com>
2021-08-09 18:37:22 +0200

    Adjusted the README

--- src/pages/FOUROFOUR.js
+++ src/pages/FOUROFOUR.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import { makeStyles, useTheme } from '@material-ui/core/styles';
-import NotFoundImage from './download.png';
 import { Typography } from '@material-ui/core';
 
 const useStyles = makeStyles((theme) => ({
